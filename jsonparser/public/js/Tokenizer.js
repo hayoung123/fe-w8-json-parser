598510c9e0742656177b868aa710920c8c532f09
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-03-30 00:08:32 +0900

    [FIX] Tokeniuzer 예외상황 추가

--- jsonparser/public/js/Tokenizer.js
+++ jsonparser/public/js/Tokenizer.js
@@ -8,11 +8,24 @@ const isSign = (value) => {
     value === ":"
   );
 };
+const isStringSign = (value) => value === "'" || value === '"';
 
 const tokenizer = (str) => {
+  const stringStack = [];
   const tokenArray = str.split("").reduce((acc, cur, idx, arr) => {
-    if (isSign(cur) || isSign(arr[idx - 1])) acc.push(cur);
-    else acc[acc.length - 1] += cur;
+    if (!stringStack.length && isStringSign(cur)) stringStack.push(cur);
+    else if (cur === stringStack[stringStack.length - 1]) stringStack.pop();
+
+    if (
+      !stringStack.length &&
+      !isStringSign(cur) &&
+      (isSign(cur) || isSign(arr[idx - 1]))
+    )
+      acc.push(cur);
+    else {
+      if (stringStack.length && isStringSign(cur)) acc.push(cur);
+      else acc[acc.length - 1] += cur;
+    }
 
     return acc;
   }, []);
@@ -24,6 +37,5 @@ const tokenizer = (str) => {
 // console.log(tokenizer(`[1,23,'hello']`));
 // console.log(tokenizer(`{a:1,b:2}`));
 //console.log(tokenizer(`[{a:1,b:2},{a:1,b:2}]`));
-console.log(tokenizer(`[{hello:[1,2],b:2},{a:'eamon',b:2}]`));
-
-
+console.log(tokenizer(`['eam[',o,"]n"]`));
+//console.log(tokenizer(`[{hello:[1,2],b:2},{a:'eam[o]n',b:2}]`));
