320dab775901ea54d49d3b29268ddce40c1406ce
kyle <67357426+hayoung123@users.noreply.github.com>
2021-03-31 22:09:14 +0900

    [EamonKyle]Json step3 (#22)
    * Feat. parser test version
    
    * Feat. array parser
    
    * Feat. json Parser

--- jsonparser/public/js/Parser.js
+++ jsonparser/public/js/Parser.js
@@ -1,29 +1,47 @@
-const preParser = (arr, stack) => {
-  if (!arr.length) return stack;
-  const cur = arr.shift();
-  console.log(cur);
-  switch (cur.type) {
-    case "openArray":
-      stack.push("Array");
+function makeNode({ type, value, subType }) {
+  if (type === 'Array' || type === 'Object' || type === 'init') return { type, child: [] };
+  else if (subType === 'propKey')
+    return { value: { propKey: { type, value }, propValue: {} }, type: 'objectProperty' };
+  else return { type, value };
+}
 
-      break;
-    case "openObject":
-      stack.push("Obj");
-
-      break;
-    case "closeArray":
-      stack.pop();
-      break;
-    case "closeObject":
-      stack.pop();
-      break;
+const preParser = (arr, node) => {
+  for (let i = 0; i < arr.length; i++) {
+    const value = arr[i];
+    switch (value.subType) {
+      case 'open':
+        const parentNode = makeNode(value);
+        const newNode = preParser(arr.slice(i + 1), parentNode);
+        i += newNode.skipIndex;
+        node.child.push(newNode.node);
+        break;
+      case 'close':
+        return { node, skipIndex: i + 1 };
+      case 'propKey':
+        const nextValue = arr[i + 1];
+        const objNode = makeNode(value);
+        const objProperyValueNode = makeNode(nextValue); //{type:array, child : []}
+        if (nextValue.type === 'Array' || nextValue.type === 'Object') {
+          const newValueNode = preParser(arr.slice(i + 2), objProperyValueNode);
+          objNode.value.propValue = newValueNode.node;
+          node.child.push(objNode);
+          i += newValueNode.skipIndex;
+        } else {
+          objNode.value.propValue = objProperyValueNode;
+          node.child.push(objNode);
+          i += 1;
+        }
+        break;
+      default:
+        node.child.push(makeNode(value));
+    }
   }
-  return preParser(arr, stack);
 };
 
 const parser = (arr) => {
-  let stack = [];
-  console.log(preParser(arr, stack));
+  const initNode = makeNode({ type: 'init' });
+  preParser(arr, initNode);
+  return initNode.child;
 };
 
 module.exports = parser;
--- jsonparser/public/js/Tokenizer.js
+++ jsonparser/public/js/Tokenizer.js
@@ -1,24 +1,22 @@
-const _ = require("./utils.js");
-const lexer = require("./Lexer.js");
-const parser = require("./Parser.js");
+const _ = require('./utils.js');
+const lexer = require('./Lexer.js');
+const parser = require('./Parser.js');
 const isSign = (value) => {
   return (
-    value === "[" ||
-    value === "," ||
-    value === "]" ||
-    value === "{" ||
-    value === "}" ||
-    value === ":"
+    value === '[' ||
+    value === ',' ||
+    value === ']' ||
+    value === '{' ||
+    value === '}' ||
+    value === ':'
   );
 };
 const isStringSign = (value) => value === "'" || value === '"';
 const isRealSign = (stringStack, value) => !stringStack.length && isSign(value);
 const isStartValue = (stringStack, value, preValue) =>
   !stringStack.length && isSign(preValue) && !isStringSign(value);
-const isStartString = (stringStack, value) =>
-  !stringStack.length && isStringSign(value);
-const isEndString = (stringStack, value) =>
-  value === stringStack[stringStack.length - 1];
+const isStartString = (stringStack, value) => !stringStack.length && isStringSign(value);
+const isEndString = (stringStack, value) => value === stringStack[stringStack.length - 1];
 const arraySpaceParser = (arr) => arr.map((v) => v.trim());
 
 const arrayQuotesParser = (arr) => arr.map(quotesParser);
@@ -26,9 +24,9 @@ const arrayQuotesParser = (arr) => arr.map(quotesParser);
 const quotesParser = (value) => {
   if (value[0] === "'" && value[value.length - 1] === "'") {
     value = value
-      .split("")
+      .split('')
       .slice(1, value.length - 1)
-      .join("");
+      .join('');
     return '"' + value + '"';
   }
   return value;
@@ -36,19 +34,15 @@ const quotesParser = (value) => {
 
 const preTokenizer = (str) => {
   const stringStack = [];
-  const tokenArray = str.split("").reduce((acc, cur, idx, arr) => {
-    if (cur === ",") return acc;
+  const tokenArray = str.split('').reduce((acc, cur, idx, arr) => {
+    if (cur === ',') return acc;
     if (isStartString(stringStack, cur)) {
       stringStack.push(cur);
       acc.push(cur);
       return acc;
     } else if (isEndString(stringStack, cur)) stringStack.pop();
 
-    if (
-      isStartValue(stringStack, cur, arr[idx - 1]) ||
-      isRealSign(stringStack, cur)
-    )
-      acc.push(cur);
+    if (isStartValue(stringStack, cur, arr[idx - 1]) || isRealSign(stringStack, cur)) acc.push(cur);
     else acc[acc.length - 1] += cur;
 
     return acc;
@@ -56,15 +50,17 @@ const preTokenizer = (str) => {
   return tokenArray;
 };
 
-const blankFilter = (arr) => arr.filter((v) => v !== "");
+const blankFilter = (arr) => arr.filter((v) => v !== '');
 
-const tokenizer = _.pipe(
-  preTokenizer,
-  arraySpaceParser,
-  arrayQuotesParser,
-  blankFilter
-);
+const tokenizer = _.pipe(preTokenizer, arraySpaceParser, arrayQuotesParser, blankFilter);
+
+module.exports = tokenizer;
 
 const main = _.pipe(tokenizer, lexer, parser);
 
-console.log(main(`[1]`));
+console.dir(
+  main(
+    '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]'
+  ),
+  { depth: null }
+);
